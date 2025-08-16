9641538454898cf2c855c3e79da63e04f39a2101
Sujin14 <sujinsuresh1422002@gmail.com>
2025-08-16 09:44:57 +0530

    removed the conflicting mainActivity.kt file codes

--- android/app/src/main/kotlin/com/sujin/musicplayer/MainActivity.kt
+++ android/app/src/main/kotlin/com/sujin/musicplayer/MainActivity.kt
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-package com.example.musicplayer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
-=======
-
 package com.sujin.musicplayer
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
->>>>>>> ae78af9e800549b81a3024a26c7f0d7b998a28ad
